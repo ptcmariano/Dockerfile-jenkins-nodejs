382c66085ba363f1fb39fc9f80db88db31c50030
Paulo Mariano <paulotiagocm@hotmail.com>
2017-05-19 13:53:59 -0300

    ajuste run

--- rund.sh
+++ rund.sh
@@ -1,1 +1,4 @@
-docker run -d --name jenkinslocal -p 8888:8080 -v /c/Users/pcastanho/Documents/My\ Web\ Sites/jenkins:/var/jenkins_home jenkinsnode
+# to build image
+docker build --name jenkinsnode .
+# to run image
+docker run -d --name jenkinslocal -p 8888:8080 -v $PWD:/var/jenkins_home jenkinsnode
